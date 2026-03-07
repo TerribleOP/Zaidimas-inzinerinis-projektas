ef8e47da6f5355844236c4cf304925d3edad4954
DovydasVait <vaidovisss@gmail.com>
2026-03-07 11:21:57 +0200

    Pridėtas minimalus mygtukų funkcionalumas
    Paspaudus mygtukus atsiranda vieta, kur bus tos srities informacija

---
+++ Assets/Scipts/UI/ButtonScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+using UnityEngine;
+
+public class TogglePanel : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void Toggle()
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
